3534d34709fc94495135e38b4cbd2877ed4e4dc3
Yousuf <yousufakhtar14@gmai.com>
2022-12-27 13:43:35 +0500

    have to add work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 
 import CollapsibleExample from './components/Header';
-import Main from './components/Main';
+import AddWorks from './components/AddWork.js';
 import Work from "./components/Modal";
 
 function App() {
+
+
+
+  
   let data = [
     {
       title: "Eat",
-      Sno: 1,
+     
       work: "Have to Eat",
     },
     {
       title: "Drink",
-      Sno: 2,
+     
       work: "Have to Drink",
     },
     {
       title: "Drink 3" ,
-      Sno: 3,
+      
       work: "Have to Drink 3",
     },
     ];
@@ -26,7 +30,7 @@ function App() {
     {/* Header */}
     <CollapsibleExample/> 
     
-    <Main/>
+   <AddWorks/>
 
  
     {
--- src/components/AddWork.js
+++ src/components/AddWork.js
@@ -1,33 +1,61 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-function Example({data}) {
- 
-
+function AddWorks({addWork}) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const pushData = () => data.push('{push}');
+  const [title, setTitle] = useState("");
+  const [work, setWork] = useState("");
+  function submit(){
+    if (!title || !work) {
+      alert("Please add some Title or Work")
+      
+    }
+   console.log({addWork})
+
+
+  }
+
+
   
 
   return (
+
     <>
-    <div className='text-center m-3'>
-      <Button  variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
+    
+      <div className="text-center m-3">
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
       </div>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title><input className="form-control form-control-lg" type="text" placeholder="Youre Work Title Here"></input></Modal.Title>
+          <Modal.Title>
+            <input
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="Youre Work Title Here"
+              value={title}
+              onChange={(e)=>{setTitle(e.target.value)}} 
+              
+            ></input>
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body><input className="form-control"  type="text" placeholder="Type Here What You Wanna do"></input></Modal.Body>
+        <Modal.Body>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Type Here What You Wanna do"
+            value={work}
+            onChange={(e)=>{setWork(e.target.value)}}
+          ></input>
+        </Modal.Body>
         <Modal.Footer>
-          
-          <Button variant="primary" onClick={pushData}>
+          <Button variant="primary" onClick={submit}>
             Add Work
           </Button>
         </Modal.Footer>
@@ -36,4 +64,4 @@ function Example({data}) {
   );
 }
 
-export default Example ;
+export default AddWorks;
